523130ace0dceedc555c0bb52f36137d98e438dc
orabyshah <bhagyeshoracle@gmail.com>
2025-04-09 15:26:03 +0530

    Commit Manage Employees Microservice v1

---
+++ ManageEmployees/src/test/java/com/example/training/manageemployees/ManageEmployeesApplicationTests.java
@@ -0,0 +1,42 @@
+package com.example.training.manageemployees;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+class ManageEmployeeApplicationTests {
+    @LocalServerPort
+    Integer serverPort; //Server port will be injected here during Testcase execution.
+	
+	@Test
+	void contextLoads() {
+	}
+
+	@Test
+	void testQueryEmployee() {
+		RestTemplate template = new RestTemplate();
+		//Invoke http://localhost:8080/employees/100 using GET
+		ResponseEntity<Employee> response = template.getForEntity("http://localhost:"+serverPort+"/employees/100", Employee.class);
+		//Checking if the response object name is 'James Cooper' ==> If yes, then case is passed.
+		assertEquals(response.getBody().getEmpName(), "James Cooper");		
+	} 
+	@Test
+	void testQueryEmployeenotFound() {
+		RestTemplate template = new RestTemplate();
+		//Invoke http://localhost:8080/employees/500 using GET
+		ResponseEntity<Employee> response = null;
+		try { 
+		 response = template.getForEntity("http://localhost:"+serverPort+"/employees/500", Employee.class);
+		}
+		catch (Exception e){
+			//System.out.println(e);
+			assertEquals(e.getMessage(), "404 : \"Employee with given id not found\"");
+		}
+	} 
+}
